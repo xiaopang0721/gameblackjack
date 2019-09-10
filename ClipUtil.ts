9f6d5705780af79fead9a4027d864e80616e45fa
xiaoer <xiaoer@example.com>
2019-09-10 19:20:36 +0800

    提交资源

--- ClipUtil.ts
+++ ClipUtil.ts
@@ -57,7 +57,7 @@ module gameblackjack {
             //场景下注数字
             this.MAP_XZ = {
                 source: Path_game_blackjack.atlas_game_ui + "zhajinhua.atlas",
-                url: Path_game_blackjack.ui_blackjack + "clip_gz.png",
+                url: Path_game_blackjack.ui_blackjack + "clip_zd.png",
                 clipWidth: 22,
                 clipHeight: 28,
                 clipX: 11,
