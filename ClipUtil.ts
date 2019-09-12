2fbc2028ef3dde783b610116e1aa5ace9d402016
xiaoer <xiaoer@example.com>
2019-09-12 16:19:10 +0800

    字体间隔

--- ClipUtil.ts
+++ ClipUtil.ts
@@ -97,7 +97,7 @@ module gameblackjack {
                 clipWidth: 22,
                 clipHeight: 28,
                 clipX: 11,
-                space: -3
+                space: -8
             };
 
 
