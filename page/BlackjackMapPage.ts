b2d29a6875893d25aa70a5718d547a1c138fa07e
xiaoer <xiaoer@example.com>
2019-10-18 19:52:07 +0800

    修改 充值按钮增加api判断

--- page/BlackjackMapPage.ts
+++ page/BlackjackMapPage.ts
@@ -135,6 +135,9 @@ module gameblackjack.page {
         // 页面打开时执行函数
         protected onOpen(): void {
             super.onOpen();
+            //api充值不显示
+            this._viewUI.btn_chongzhi.visible = !WebConfig.enterGameLocked;
+            
             this.updateViewUI();
             this.onUpdateUnitOffline();
             if (!this._blackjackMgr.isReLogin) {
