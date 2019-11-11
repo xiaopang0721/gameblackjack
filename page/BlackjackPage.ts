0a217ebd5a72e838a0c3e9458cecfb53fa64a88b
happy <happy>
2019-11-11 13:12:46 +0800

    缓动时不要点击

--- page/BlackjackPage.ts
+++ page/BlackjackPage.ts
@@ -68,14 +68,19 @@ module gameblackjack.page {
 				this._viewUI.box_right._childs[index].visible = true;
 				Laya.Tween.from(this._viewUI.box_right._childs[index], {
 					right: -300
-				}, 200 + index * 100, Laya.Ease.linearNone);
+				}, this._initialtime + index * this._time, Laya.Ease.linearNone);
 			}
+			Laya.timer.once(this._initialtime + 4 * this._time, this, this.onComplete)
+			this._viewUI.btn_join.on(LEvent.CLICK, this, this.onBtnClickWithTween);
+		}
 
+		private _initialtime: number = 200;
+		private _time: number = 100;
+		private onComplete(){
 			this._viewUI.img_room0.on(LEvent.CLICK, this, this.onBtnClickWithTween);
 			this._viewUI.img_room1.on(LEvent.CLICK, this, this.onBtnClickWithTween);
 			this._viewUI.img_room2.on(LEvent.CLICK, this, this.onBtnClickWithTween);
 			this._viewUI.img_room3.on(LEvent.CLICK, this, this.onBtnClickWithTween);
-			this._viewUI.btn_join.on(LEvent.CLICK, this, this.onBtnClickWithTween);
 		}
 
 		protected onBtnTweenEnd(e: LEvent, target: any): void {
